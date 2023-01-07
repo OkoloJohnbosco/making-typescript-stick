eee5a83abfe2437f99e4265f70be67c5d4077f4d
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-01-07 22:14:45 +0100

    learn: using a fxn in super call

--- challenges/data-layer/src/index.ts
+++ challenges/data-layer/src/index.ts
@@ -140,3 +140,23 @@ enum Linter {
   JSLint = 3,
   JSHint,
 }
+
+/* This works
+class Person {
+  #name: string;
+  constructor(name: string) {
+    this.#name = name;
+  }
+}
+function makeName(name: string | string[]): string {
+  if (Array.isArray(name)) return name.join(" ");
+  else return name;
+}
+class Student extends Person {
+  #name: string | string[];
+  constructor(name: string | string[]) {
+    super(makeName(name));
+    this.#name = name;
+  }
+}
+And ends here */
